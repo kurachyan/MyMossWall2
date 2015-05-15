76ca9b3c91dce06c33020533734490b405298253
kurachyan <e_tamakou@outlook.jp>
2015-05-16 00:49:32 +0900

    [Dictionary]枠作成１

---
+++ MossDictionary/CS_MossDictionary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MossImage;
+
+namespace MossDictionary
+{
+    public class CS_MossDictionary
+    {
+    }
+}
