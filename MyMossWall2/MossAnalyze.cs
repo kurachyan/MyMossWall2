76badda1d7c1a9e4f52f6e854b4588b6259211d8
kurachyan <e_tamakou@outlook.jp>
2015-05-18 13:43:39 +0900

    [Analyze]枠組み作成２

--- MyMossWall2/MossAnalyze.cs
+++ MyMossWall2/MossAnalyze.cs
@@ -4,12 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using MossTable;
+using MossDictionary;
+
 namespace MyMossWall2
 {
     class MossAnalyze
     {
+        CS_MossTable MT;           // MossTable
+        CS_MossDictionary MD;      // MossDictionary
+
+        private Stack<string> Stack;      // 解析スタック
+
         public MossAnalyze()
         {   // コンストラクタ
+            Stack = new Stack<string>();
         }
+
+        public void SetMT(CS_MossTable _mt)
+        {
+            this.MT = _mt;
+        }
+        public void SetMD(CS_MossDictionary _md)
+        {
+            this.MD = _md;
+        }
+
+        public void Init()
+        {   // 初期情報設定
+            Stack.Clear();
+        }
+
+        public void Add(String _Value)
+        {   // 要素追加
+            Stack.Push(_Value);
+        }
+
+//        public void Delete(String _Name)
+//        {   // 要素削除
+//        }
+
+//        public Boolean Check(String _Name)
+//        {   // 要素確認
+//        }
+
+//        public String Value(String _Name)
+//        {
+//        }
+
+//        public String Get(int _Pos)
+//        {
+//        }
     }
 }
